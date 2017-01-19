2f8729b95dff3c799382717e6d4a899d6525992a
franco83-2 <franco.84@gmail.com>
2017-01-19 09:52:24 -0500

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  
+  def initalize
+  end
+
+  def answer
+   a = 999
+   b = 999
+   max_palindrome = 1
+
+   while a > 0
+     b = 999
+     while b > 0
+       product = a * b
+         if product.to_s == product.to_s.reverse
+           max_palindrome = product if product > max_palindrome
+         end
+       b -= 1
+     end
+     a -= 1
+   end
+   max_palindrome
+  end
+
+ end
